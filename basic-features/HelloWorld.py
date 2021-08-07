84457c4e559c9e77900db284df3454c88fbf930b
Rushikesh Teli <rushikeshteli@Rushikeshs-MacBook-Pro.local>
2021-08-07 15:37:11 +1000

    Initial Commit

---
+++ basic-features/HelloWorld.py
@@ -0,0 +1,12 @@
+# Basic Language Features
+print ("Hello rushikesh")
+''' This is a multiline comment in python. The main objective of this code is
+ to be able to demonstrate as many features of language as possible'''
+
+a = 2+3
+b = ["Hello", "Rushikesh"];
+print(b)
+print(b[1])
+
+c={"name":"Rushikesh", "profession": "developer"};
+print(c["name"] + " does " + c["profession"] + " job.");
